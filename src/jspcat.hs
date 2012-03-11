7eb0f1f4e8b675078fdded79681252dd8aefed7b
Anton Ekblad <anton@ekblad.cc>
2012-03-11 11:58:34 +0100

    Add jspcat executable, for looking at the intermediate JSMod files.

---
+++ src/jspcat.hs
@@ -0,0 +1,13 @@
+-- | jspcat - print the contents of a JSMod intermediate file.
+module Main where
+import System.Environment (getArgs)
+import CodeGen.Javascript
+
+main = do
+  as <- getArgs
+  if null as
+     then putStrLn "Usage: jspcat Mod1.jsmod [Mod2.jsmod, ...]"
+     else mapM_ catMod as
+  where
+    catMod m =
+      readModule m >>= return . prettyMod >>= putStrLn
