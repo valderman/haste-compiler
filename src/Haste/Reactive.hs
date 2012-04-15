865ac75e2dc7852313b1afc3ef0fb4b4fdf5436d
Anton Ekblad <anton@ekblad.cc>
2012-04-15 11:01:20 +0200

    Export FRP.Fursuit as part of Haste.Reactive.

--- src/Haste/Reactive.hs
+++ src/Haste/Reactive.hs
@@ -1,3 +1,4 @@
-module Haste.Reactive (module DOM, module Ajax) where
+module Haste.Reactive (module Fursuit, module DOM, module Ajax) where
+import FRP.Fursuit as Fursuit
 import Haste.Reactive.DOM as DOM
 import Haste.Reactive.Ajax as Ajax
