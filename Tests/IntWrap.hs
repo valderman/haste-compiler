0835f6f2a099a83aebec894ef68e72ed8be3659d
Anton Ekblad <anton@ekblad.cc>
2012-05-08 15:52:36 +0200

    We have to compare Haste Int with GHC Int32, in case we're on 64 bit!

--- Tests/IntWrap.hs
+++ Tests/IntWrap.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE CPP #-}
 module Tests.IntWrap where
+import Data.Int
+
+#ifdef HASTE
+type Intish = Int
+#else
+type Intish = Int32
+#endif
 
 {-# NOINLINE int_max #-}
-int_max :: Int
+int_max :: Intish
 int_max = 0x7fffffff
 
 {-# NOINLINE int_min #-}
-int_min :: Int
+int_min :: Intish
 int_min = -2147483648
 
 {-# NOINLINE num #-}
-num :: Int
+num :: Intish
 num = 0xfffff
 
-runTest :: IO [Int]
+runTest :: IO [Intish]
 runTest = return [
   int_max + 27,
   int_min - 1,
