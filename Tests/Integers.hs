a67b1f408b38ae4927826aa5e1fc8a1c8f04a557
Anton Ekblad <anton@ekblad.cc>
2012-05-08 22:28:30 +0200

    Disallow Integer literals outside the range +/- 2^31 for now.

--- Tests/Integers.hs
+++ Tests/Integers.hs
@@ -1,13 +1,16 @@
 module Tests.Integers where
 import Data.Bits
 
+-- TODO: at the moment, it's only possible to specify signed 32 bit integer
+--       literals!
+
 {-# NOINLINE num1 #-}
 num1 :: Integer
-num1 = 0xffffffffffff
+num1 = 0xfffffff
 
 {-# NOINLINE num2 #-}
 num2 :: Integer
-num2 = 4234509348702334235435346
+num2 = 423450934
 
 runTest :: IO [Integer]
 runTest = return [
@@ -15,4 +18,4 @@ runTest = return [
   num1 + num2,
   num1 * num2,
   num2 `div` num1,
-  (num2 `xor` num1) .&. 43042900409]
+  (num2 `xor` num1) .&. 43042900]
