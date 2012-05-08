360a6d7016e07c113b6413d07e336b5a3ec29950
Anton Ekblad <anton@ekblad.cc>
2012-05-09 00:32:39 +0200

    Add test case for Double division.

---
+++ Tests/DoubleDiv.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE CPP #-}
+module Tests.DoubleDiv where
+
+#ifdef HASTE
+import Haste
+str = show_ . (round_ :: Double -> Int)
+#else
+str = show . (round :: Double -> Int)
+#endif
+
+{-# NOINLINE a #-}
+a :: Double
+a = 1000000000
+
+{-# NOINLINE b #-}
+b :: Double
+b = 34029340939
+
+{-# NOINLINE c #-}
+c :: Double
+c = 234
+
+runTest :: IO [String]
+runTest = return [
+  str (a/a), str (a/b), str (a/c),
+  str (b/a), str (b/b), str (b/c),
+  str (c/a), str (c/b), str (c/c)]
