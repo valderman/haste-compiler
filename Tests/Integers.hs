bf95876112c35f2c7d297ef4c3c1a8cef0501bcf
Anton Ekblad <anton@ekblad.cc>
2012-05-13 11:06:09 +0200

    Improve Integers test case a tiny bit.

--- Tests/Integers.hs
+++ Tests/Integers.hs
@@ -12,10 +12,19 @@ num1 = 0xfffffff
 num2 :: Integer
 num2 = 423450934
 
+{-# NOINLINE num3 #-}
+num3 :: Integer
+num3 = 50000
+
+{-# NOINLINE num4 #-}
+num4 :: Integer
+num4 = num3*num3+num2
+
 runTest :: IO [Integer]
 runTest = return [
   num1,
   num1 + num2,
   num1 * num2,
   num2 `div` num1,
-  (num2 `xor` num1) .&. 43042900]
+  (num2 `xor` num1) .&. 43042900,
+  num4]
