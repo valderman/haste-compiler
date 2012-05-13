90dc07720940716bbb55546fe6a434c706669c13
Anton Ekblad <anton@ekblad.cc>
2012-05-13 11:06:23 +0200

    Fix test case for tailcalls. Forgot that it uses Integer, not Int32.

--- Tests/Tailcalls.hs
+++ Tests/Tailcalls.hs
@@ -6,13 +6,17 @@ mysum :: Integer -> [Integer] -> Integer
 mysum n (x:xs) = mysum (n+x) xs
 mysum n _      = n
 
+{-# NOINLINE num #-}
+num :: Integer
+num = 70711
+
 {-# NOINLINE test #-}
 test :: Integer
+#if !defined(TCE) && defined(HASTE)
+test = num^2+4479
+#else
 test = mysum 0 [1..100000]
+#endif
 
 runTest :: IO Integer
-#if !defined(TCE) && defined(HASTE)
-runTest = return 705082704
-#else
 runTest = return test
-#endif 
