366dd065c8e649bf013d66fe03160d73a04066ac
Anton Ekblad <anton@ekblad.cc>
2012-05-29 14:35:11 +0200

    Add more functions for adding and removing child DOM nodes.

--- src/Haste/DOM.hs
+++ src/Haste/DOM.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Haste.DOM (Elem (..), PropID, ElemID,
                   newElem, elemById, setProp, getProp, withElem,
-                  addChild, removeChild, clearChildren,
+                  addChild, addChildBefore, removeChild, clearChildren,
+                  getChildBefore, getLastChild, getChildren, setChildren,
                   getStyle, setStyle) where
 import Haste.Prim
 
@@ -16,12 +17,42 @@ foreign import ccall jsSetStyle :: Elem -> JSString -> JSString -> IO ()
 foreign import ccall jsFind :: JSString -> IO (Ptr (Maybe Elem))
 foreign import ccall jsCreateElem :: JSString -> IO Elem
 foreign import ccall jsAppendChild :: Elem -> Elem -> IO ()
+foreign import ccall jsGetLastChild :: Elem -> IO (Ptr (Maybe Elem))
+foreign import ccall jsGetChildren :: Elem -> IO (Ptr [Elem])
+foreign import ccall jsSetChildren :: Elem -> Ptr [Elem] -> IO ()
+foreign import ccall jsAddChildBefore :: Elem -> Elem -> Elem -> IO ()
+foreign import ccall jsGetChildBefore :: Elem -> IO (Ptr (Maybe Elem))
 foreign import ccall jsKillChild :: Elem -> Elem -> IO ()
 foreign import ccall jsClearChildren :: Elem -> IO ()
 
 -- | Append the first element as a child of the second element.
 addChild :: Elem -> Elem -> IO ()
 addChild = jsAppendChild
+
+-- | Insert the first element as a child into the second, before the third.
+--   For instance:
+-- @
+--   addChildBefore childToAdd theContainer olderChild
+-- @
+addChildBefore :: Elem -> Elem -> Elem -> IO ()
+addChildBefore = jsAddChildBefore
+
+-- | Get the sibling before the given one, if any.
+getChildBefore :: Elem -> IO (Maybe Elem)
+getChildBefore e = fromPtr `fmap` jsGetChildBefore e
+
+-- | Get the last of an element's children.
+getLastChild :: Elem -> IO (Maybe Elem)
+getLastChild e = fromPtr `fmap` jsGetLastChild e
+
+-- | Get a list of all children belonging to a certain element.
+getChildren :: Elem -> IO [Elem]
+getChildren e = fromPtr `fmap` jsGetChildren e
+
+-- | Clear the given element's list of children, and append all given children
+--   to it.
+setChildren :: Elem -> [Elem] -> IO ()
+setChildren e ch = jsSetChildren e (toPtr ch)
 
 -- | Create an element.
 newElem :: String -> IO Elem
