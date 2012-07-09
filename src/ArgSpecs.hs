f6f899da4f3dd30ba87dda573a5f415560ab2315
Anton Ekblad <anton@ekblad.cc>
2012-07-09 13:28:34 +0200

    List args in alphabetical order when passing --help.

--- src/ArgSpecs.hs
+++ src/ArgSpecs.hs
@@ -10,18 +10,25 @@ argSpecs = [
               updateCfg = \cfg _ -> cfg {ppOpts  = pretty},
               info = "Output indented, fairly readable code, with all " ++
                      "external names included in comments."},
-    ArgSpec { optName = "start=asap",
-              updateCfg = \cfg _ -> cfg {appStart = startASAP},
-              info = "Start program immediately instead of on document load."},
-    ArgSpec { optName = "out=",
-              updateCfg = \cfg outfile -> cfg {outFile = const $ head outfile},
-              info = "Write the JS blob to <arg>."},
     ArgSpec { optName = "libinstall",
               updateCfg = \cfg _ -> cfg {targetLibPath = sysLibPath,
                                          performLink   = False},
               info = "Install all compiled modules into the user's jsmod "
                      ++ "library\nrather than linking them together into a JS"
                      ++ "blob."},
+    ArgSpec { optName = "opt-all",
+              updateCfg = updateClosureCfg,
+              info = "Enable all safe optimizations. "
+                     ++ "Equivalent to -O2 --opt-google-closure."},
+    ArgSpec { optName = "opt-google-closure",
+              updateCfg = updateClosureCfg,
+              info = "Run the Google Closure compiler on the output. "
+                   ++ "Use --opt-google-closure=foo.jar to hint that foo.jar "
+                   ++ "is the Closure compiler."},
+    ArgSpec { optName = "opt-tce",
+              updateCfg = \cfg _ -> cfg {doTCE = True,
+                                         evalLib = evalTrampolining},
+              info = "Perform tail call elimination."},
     ArgSpec { optName = "opt-vague-ints",
               updateCfg = \cfg _ -> cfg {wrapIntMath = id},
               info = "Int math has 53 bits of precision, but gives incorrect "
@@ -30,19 +37,12 @@ argSpecs = [
                      ++ "only work on the lowest 32 bits. This option should "
                      ++ "give a substantial performance boost for Int math "
                      ++ "heavy code."},
-    ArgSpec { optName = "opt-tce",
-              updateCfg = \cfg _ -> cfg {doTCE = True,
-                                         evalLib = evalTrampolining},
-              info = "Perform tail call elimination."},
-    ArgSpec { optName = "opt-google-closure",
-              updateCfg = updateClosureCfg,
-              info = "Run the Google Closure compiler on the output. "
-                   ++ "Use --opt-google-closure=foo.jar to hint that foo.jar "
-                   ++ "is the Closure compiler."},
-    ArgSpec { optName = "opt-all",
-              updateCfg = updateClosureCfg,
-              info = "Enable all safe optimizations. "
-                     ++ "Equivalent to -O2 --opt-google-closure."},
+    ArgSpec { optName = "out=",
+              updateCfg = \cfg outfile -> cfg {outFile = const $ head outfile},
+              info = "Write the JS blob to <arg>."},
+    ArgSpec { optName = "start=asap",
+              updateCfg = \cfg _ -> cfg {appStart = startASAP},
+              info = "Start program immediately instead of on document load."},
     ArgSpec { optName = "verbose",
               updateCfg = \cfg _ -> cfg {verbose = True},
               info = "Display even the most obnoxious warnings."},
