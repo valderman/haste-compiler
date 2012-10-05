313be0d133841af26e2c7b247c6093b6f3a1acc2
Anton Ekblad <anton@ekblad.cc>
2012-10-05 12:24:22 +0200

    Enable installing .jsmods only, by running haste-inst build --install-jsmods.

--- src/haste-inst.hs
+++ src/haste-inst.hs
@@ -8,10 +8,26 @@ cabal :: [String] -> IO ()
 cabal args = do
   runAndWait "cabal" (hasteargs ++ args) Nothing
   where
-    hasteargs = ["--with-compiler=" ++ hasteBinary,
-                 "--with-hc-pkg=haste-pkg",
-                 "--prefix=" ++ hasteDir </> "haste-install",
-                 "--ghc-options=\"--libinstall\""]
+    hasteargs 
+      | "build" `elem` args =
+        ["--with-ghc=" ++ hasteBinary]
+      | otherwise =
+        ["--with-compiler=" ++ hasteBinary,
+         "--with-hc-pkg=haste-pkg",
+         "--prefix=" ++ hasteDir </> "haste-install"]
 
 main :: IO ()
-main = getArgs >>= cabal
+main = do
+  as <- getArgs
+  cabal (hasteargs as)
+  where
+    libinstall = "--ghc-options=\"--libinstall\""
+    hasteargs as
+    -- If the user passes --install-jsmods, hastec is invoked with --libinstall
+    -- even if running haste-inst build.
+      | "--install-jsmods" `elem` as =
+        libinstall : filter (/= "--install-jsmods") as
+      | "build" `elem` as =
+        as
+      | otherwise =
+        libinstall : as
