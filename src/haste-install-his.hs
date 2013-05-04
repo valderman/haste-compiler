e373147db9b520fc13e193aa777c9b965948e61d
Anton Ekblad <anton@ekblad.cc>
2013-05-04 14:50:56 +0200

    Add haste-install-his, a tool to install .hi files for a package.
    Somehow I forgot to add this before. Oops!

---
+++ src/haste-install-his.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TupleSections #-}
+-- | haste-install-his; install all .hi files in a directory.
+module Main where
+import EnvUtils
+import System.FilePath
+import System.Directory
+import System.Environment
+import Control.Applicative
+import Control.Monad
+import Data.List
+import Data.Char
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [package, dir] -> installFromDir (libDir </> package) dir
+    _              -> putStrLn "Usage: haste-install-his pkgname dir"
+
+getHiFiles :: FilePath -> IO [FilePath]
+getHiFiles dir =
+  filter (".hi" `isSuffixOf`) <$> getDirectoryContents dir
+
+getSubdirs :: FilePath -> IO [FilePath]
+getSubdirs dir = do
+  contents <- getDirectoryContents dir
+  someDirs <- mapM (\d -> (d,) <$> doesDirectoryExist (dir </> d)) contents
+  return [path | (path, isDir) <- someDirs
+               , isDir
+               , head path /= '.'
+               , isUpper (head path)]
+
+installFromDir :: FilePath -> FilePath -> IO ()
+installFromDir base path = do
+  hiFiles <- getHiFiles path
+  when (not $ null hiFiles) $ do
+    createDirectoryIfMissing True (libDir </> base)
+  mapM_ (installHiFile base path) hiFiles
+  getSubdirs path >>= mapM_ (\d -> installFromDir (base </> d) (path </> d))
+
+installHiFile :: FilePath -> FilePath -> FilePath -> IO ()
+installHiFile to from file = do
+  putStrLn $ "Installing " ++ from </> file ++ "..."
+  copyFile (from </> file) (to </> file)
