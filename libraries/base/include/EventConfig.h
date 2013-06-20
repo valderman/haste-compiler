c0eb0acc0f787245bfc85294cb0d15c7de62c279
Anton Ekblad <anton@ekblad.cc>
2013-06-20 11:43:42 +0200

    Get rid of epoll and kqueue entirely.

--- libraries/base/include/EventConfig.h
+++ libraries/base/include/EventConfig.h
@@ -1,29 +1,8 @@
 /* include/EventConfig.h.  Generated from EventConfig.h.in by configure.  */
 /* include/EventConfig.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define if you have epoll support. */
-#define HAVE_EPOLL 1
-
-/* Define to 1 if you have the `epoll_create1' function. */
-/* #undef HAVE_EPOLL_CREATE1 */
-
-/* Define to 1 if you have the `epoll_ctl' function. */
-#define HAVE_EPOLL_CTL 1
-
-/* Define to 1 if you have the `eventfd' function. */
-#define HAVE_EVENTFD 1
-
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the `kevent' function. */
-/* #undef HAVE_KEVENT */
-
-/* Define to 1 if you have the `kevent64' function. */
-/* #undef HAVE_KEVENT64 */
-
-/* Define if you have kqueue support. */
-/* #undef HAVE_KQUEUE */
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
@@ -49,15 +28,6 @@
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-/* Define to 1 if you have the <sys/epoll.h> header file. */
-#define HAVE_SYS_EPOLL_H 1
-
-/* Define to 1 if you have the <sys/eventfd.h> header file. */
-#define HAVE_SYS_EVENTFD_H 1
-
-/* Define to 1 if you have the <sys/event.h> header file. */
-/* #undef HAVE_SYS_EVENT_H */
-
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
 
@@ -68,13 +38,13 @@
 #define HAVE_UNISTD_H 1
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "libraries@haskell.org"
+#define PACKAGE_BUGREPORT "http://github.com/valderman/haste-compiler"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "Haskell base package"
+#define PACKAGE_NAME "Haste base package"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Haskell base package 1.0"
+#define PACKAGE_STRING "Haste base package 1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "base"
@@ -84,9 +54,3 @@
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* The size of `kev.filter', as computed by sizeof. */
-/* #undef SIZEOF_KEV_FILTER */
-
-/* The size of `kev.flags', as computed by sizeof. */
-/* #undef SIZEOF_KEV_FLAGS */
