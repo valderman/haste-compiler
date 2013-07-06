2391cf133d6881a7bdd83cab3f5473dffd926246
Anton Ekblad <anton@ekblad.cc>
2013-07-06 19:13:06 +0200

    Fix typo in packagelibs.sh.

--- packagelibs.sh
+++ packagelibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "" ]] ; then
-    echo "Usage: ./buildlibs.sh version"
+    echo "Usage: ./packagelibs.sh version"
 else
     tar -cjf haste-libs-$1.tar.bz2 buildlibs.sh include libraries/{array,base,fursuit,ghc-prim,haste-lib,integer-gmp,rts.pkg}
 fi
