a5e4b35a5c117f08dbad42f60409dab47ba15a55
Anton Ekblad <anton@ekblad.cc>
2013-07-07 22:21:08 +0200

    Add a wait function to Haste.Concurrent.

--- libraries/haste-lib/src/Haste/Concurrent.hs
+++ libraries/haste-lib/src/Haste/Concurrent.hs
@@ -1,4 +1,12 @@
 -- | Concurrency for Haste. Includes MVars, forking, Ajax and more.
-module Haste.Concurrent (module Monad, module Ajax) where
+module Haste.Concurrent (module Monad, module Ajax, wait) where
 import Haste.Concurrent.Monad as Monad
 import Haste.Concurrent.Ajax as Ajax
+import Haste.Callback
+
+-- | Wait for n milliseconds.
+wait :: Int -> CIO ()
+wait ms = do
+  v <- newEmptyMVar
+  liftIO $ setTimeout' ms $ putMVar v ()
+  takeMVar v
