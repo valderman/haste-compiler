5108826d3cab7c0f950b69700dbdcd834fe1a7ee
Anton Ekblad <anton@ekblad.cc>
2013-07-25 00:46:12 +0200

    Bump version to 0.2.2.

--- src/Haste/Version.hs
+++ src/Haste/Version.hs
@@ -10,7 +10,7 @@ import Config (cProjectVersion)
 import Haste.Environment (hasteDir)
 
 hasteVersion :: Version
-hasteVersion = Version [0, 2, 1] []
+hasteVersion = Version [0, 2, 2] []
 
 ghcVersion :: String
 ghcVersion = cProjectVersion
