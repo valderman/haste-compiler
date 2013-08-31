210a418ebfc5b65426c98eeeb4c8dc66e37e086f
Anton Ekblad <anton@ekblad.cc>
2013-08-31 12:48:12 +0200

    Add FFI example. Closes #83.

---
+++ examples/ffi/ffi.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+-- | This example implements a really simple JQuery wrapper, to demonstrate
+--   how foreign imports work.
+--
+--   For more information, see doc/js-externals.txt.
+module Main where
+import Haste
+import Control.Monad (when)
+
+newtype JQuery = JQuery JSAny
+
+foreign import ccall js_jquery :: JSString -> IO (JQuery)
+foreign import ccall js_click :: JQuery -> JSFun (Int -> IO ()) -> IO ()
+foreign import ccall js_hide :: JQuery -> IO ()
+
+-- | Since we can't name it '$', let's just call it 'j'.
+j :: String -> (JQuery -> IO ()) -> IO ()
+j s action = js_jquery (toJSString s) >>= action
+
+-- | Register an onclick callback.
+click :: (Int -> IO ()) -> JQuery -> IO ()
+click f jq = js_click jq (mkCallback f)
+
+-- | Hide an element.
+hide :: JQuery -> IO ()
+hide jq = js_hide jq
+
+main = do
+  j "#closeBlack" $ click (\button -> when (button == 0) (j "#blackBox" $ hide))
+  j "#closeRed" $ click (\button -> when (button == 0) (j "#redBox" $ hide))
---
+++ examples/ffi/wrapper.js
@@ -0,0 +1,19 @@
+// jQuery object selector. Note that the FFI marshals Int, Double, JSString,
+// Ptr-wrapped types and other primitives for us, so there's no need to do
+// anything special with such arguments or return values.
+function js_jquery(str) {
+    return $(str);
+}
+
+function js_click(obj, callback) {
+    // Callbacks must be called using the A() Haste RTS operation, taking a
+    // function and an array of arguments. Note that arguments must be
+    // explicitly wrapped ([0, evt.button] rather than just evt.button) and
+    // that the argument list must always be end with a dummy argument;
+    // in this case 0.
+    obj.click(function(evt) {A(callback, [[0, evt.button], 0]);});
+}
+
+function js_hide(obj) {
+    obj.hide();
+}
