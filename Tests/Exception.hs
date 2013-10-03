a4300bf6de85000bedc683cfeb512e3e1bbd3896
Anton Ekblad <anton@ekblad.cc>
2013-10-03 10:27:03 +0200

    Fix Exception test case for GHC 7.4.

--- Tests/Exception.hs
+++ Tests/Exception.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Tests.Exception where
-import Prelude hiding (catch)
+import Prelude
 import Data.Typeable
-import Control.Exception
+import Control.Exception as E
 
 newtype TestException = TE String deriving (Show, Typeable)
 instance Exception TestException
@@ -12,6 +12,6 @@ printTE (TE s) = return s
 
 runTest :: IO String
 runTest = do
-  a <- catch (throw $ TE "So I heard you like ") printTE
-  b <- catch (return "mudkips...") printTE
+  a <- E.catch (throw $ TE "So I heard you like ") printTE
+  b <- E.catch (return "mudkips...") printTE
   return (a ++ b)
