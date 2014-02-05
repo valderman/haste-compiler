441b5926b510a9befb70c29fb5b81d60c5aac6a7
Anton Ekblad <anton@ekblad.cc>
2014-02-05 23:02:40 +0100

    Export mkUseful from Haste.App.

--- libraries/haste-lib/src/Haste/App.hs
+++ libraries/haste-lib/src/Haste/App.hs
@@ -1,7 +1,7 @@
 -- | Type-safe client-server communication for Haste.
 module Haste.App (
   Exportable, App, Server, Useless, Export, Done,
-  liftServerIO, export, runApp, (<.>),
+  liftServerIO, export, runApp, (<.>), mkUseful,
   AppCfg, cfgURL, cfgPort, defaultConfig,
   Client,
   runClient, onServer, liftIO, liftCIO) where
