e5c223db5e06bb18d5fde07a58acf14e869a786c
Anton Ekblad <anton@ekblad.cc>
2014-02-05 23:12:24 +0100

    Export Serialize class from Haste.App.

--- libraries/haste-lib/src/Haste/App.hs
+++ libraries/haste-lib/src/Haste/App.hs
@@ -1,9 +1,10 @@
 -- | Type-safe client-server communication for Haste.
 module Haste.App (
-  Exportable, App, Server, Useless, Export, Done,
+  Serialize, Exportable, App, Server, Useless, Export, Done,
   liftServerIO, export, runApp, (<.>), mkUseful,
   AppCfg, cfgURL, cfgPort, defaultConfig,
   Client,
   runClient, onServer, liftIO, liftCIO) where
 import Haste.App.Client
 import Haste.App.Monad
+import Haste.Serialize
