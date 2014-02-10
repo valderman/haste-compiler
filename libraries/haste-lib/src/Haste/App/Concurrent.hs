8ce6e2c625b600e6ad4db22f57e953eb2081ef52
Anton Ekblad <anton@ekblad.cc>
2014-02-10 17:20:59 +0100

    Add event-based server abstraction to Haste.Concurrent, Haste.App.Concurrent.

--- libraries/haste-lib/src/Haste/App/Concurrent.hs
+++ libraries/haste-lib/src/Haste/App/Concurrent.hs
@@ -6,7 +6,8 @@
 --   decent native concurrency support.
 module Haste.App.Concurrent (
     C.MVar,
-    fork, forkMany, newMVar, newEmptyMVar, takeMVar, putMVar, peekMVar
+    fork, forkMany, newMVar, newEmptyMVar, takeMVar, putMVar, peekMVar,
+    server
   ) where
 import qualified Haste.Concurrent.Monad as C
 import Haste.App.Client
@@ -21,15 +22,21 @@ fork m = do
 forkMany :: [Client ()] -> Client ()
 forkMany = mapM_ fork
 
+-- | Create a new MVar with the specified contents.
 newMVar :: a -> Client (C.MVar a)
 newMVar = liftCIO . C.newMVar
 
+-- | Create a new empty MVar.
 newEmptyMVar :: Client (C.MVar a)
 newEmptyMVar = liftCIO C.newEmptyMVar
 
+-- | Read the value of an MVar. If the MVar is empty, @takeMVar@ blocks until
+--   a value arrives. @takeMVar@ empties the MVar.
 takeMVar :: C.MVar a -> Client a
 takeMVar = liftCIO . C.takeMVar
 
+-- | Put a value into an MVar. If the MVar is full, @putMVar@ will block until
+--   the MVar is empty.
 putMVar :: C.MVar a -> a -> Client ()
 putMVar v x = liftCIO $ C.putMVar v x
 
@@ -37,3 +44,23 @@ putMVar v x = liftCIO $ C.putMVar v x
 --   If the MVar is empty, @peekMVar@ immediately returns @Nothing@.
 peekMVar :: C.MVar a -> Client (Maybe a)
 peekMVar = liftCIO . C.peekMVar
+
+-- | Creates a generic server thread. A server is a function taking a state
+--   and an event argument, returning an updated state or Nothing.
+--   @server@ creates an MVar that is used to pass events to the server.
+--   Whenever a value is written to this MVar, that value is passed to the
+--   server function togeter with its current state.
+--   If the server function returns Nothing, the server thread terminates.
+--   If it returns a new state, the server again blocks on the event MVar,
+--   and will use the new state to any future calls to the server function.
+server :: state -> (state -> evt -> Client (Maybe state)) -> Client (C.MVar evt)
+server initialState handler = do
+    evtvar <- newEmptyMVar
+    fork $ loop evtvar initialState
+    return evtvar
+  where
+    loop m st = do
+      mresult <- takeMVar m >>= handler st
+      case mresult of
+        Just st' -> loop m st'
+        _        -> return ()
--- libraries/haste-lib/src/Haste/Concurrent.hs
+++ libraries/haste-lib/src/Haste/Concurrent.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeFamilies #-}
 -- | Concurrency for Haste. Includes MVars, forking, Ajax and more.
-module Haste.Concurrent (module Monad, module Ajax, wait) where
+module Haste.Concurrent (module Monad, module Ajax, wait, server) where
 import Haste.Concurrent.Monad as Monad
 import Haste.Concurrent.Ajax as Ajax
 import Haste.Callback
@@ -11,6 +11,26 @@ wait ms = do
   v <- newEmptyMVar
   liftIO $ setTimeout' ms $ putMVar v ()
   takeMVar v
+
+-- | Creates a generic server thread. A server is a function taking a state
+--   and an event argument, returning an updated state or Nothing.
+--   @server@ creates an MVar that is used to pass events to the server.
+--   Whenever a value is written to this MVar, that value is passed to the
+--   server function togeter with its current state.
+--   If the server function returns Nothing, the server thread terminates.
+--   If it returns a new state, the server again blocks on the event MVar,
+--   and will use the new state to any future calls to the server function.
+server :: state -> (state -> evt -> CIO (Maybe state)) -> CIO (MVar evt)
+server initialState handler = do
+    evtvar <- newEmptyMVar
+    forkIO $ loop evtvar initialState
+    return evtvar
+  where
+    loop m st = do
+      mresult <- takeMVar m >>= handler st
+      case mresult of
+        Just st' -> loop m st'
+        _        -> return ()
 
 instance GenericCallback (CIO ()) CIO where
   type CB (CIO ()) = IO ()
