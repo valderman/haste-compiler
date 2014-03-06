dc4452d1ea841e8d37532812816fdd07f87f8549
Anton Ekblad <anton@ekblad.cc>
2014-03-06 13:58:19 +0100

    Don't wrap Blobs in Haste.WebSockets.

--- libraries/haste-lib/src/Haste/WebSockets.hs
+++ libraries/haste-lib/src/Haste/WebSockets.hs
@@ -3,14 +3,16 @@
 module Haste.WebSockets (
     module Haste.Concurrent,
     WebSocket,
-    withWebSocket, wsSend
+    withWebSocket, withBinaryWebSocket, wsSend, wsSendBlob
   ) where
 import Haste
 import Haste.Foreign
-import Haste.Concurrent
+import Haste.Concurrent hiding (encode, decode)
+import Haste.Binary (Blob)
 import Unsafe.Coerce
 
 newtype WSOnMsg = WSOnMsg (WebSocket -> JSString -> IO ())
+newtype WSOnBinMsg = WSOnBinMsg (WebSocket -> Blob -> IO ())
 newtype WSComputation = WSComputation (WebSocket -> IO ())
 newtype WSOnError = WSOnError (IO ())
 
@@ -19,6 +21,9 @@ instance Marshal WebSocket where
   pack = unsafeCoerce
   unpack = unsafeCoerce
 instance Marshal WSOnMsg where
+  pack = unsafeCoerce
+  unpack = unsafeCoerce
+instance Marshal WSOnBinMsg where
   pack = unsafeCoerce
   unpack = unsafeCoerce
 instance Marshal WSComputation where
@@ -47,6 +52,25 @@ withWebSocket url cb err f = do
   where
     cb' = WSOnMsg $ \ws msg -> concurrent $ cb ws msg
 
+-- | Run a computation with a web socket. The computation will not be executed
+--   until a connection to the server has been established.
+withBinaryWebSocket :: URL
+              -- ^ URL to bind the WebSocket to
+              -> (WebSocket -> Blob -> CIO ())
+              -- ^ Computation to run when new data arrives
+              -> CIO a
+              -- ^ Computation to run when an error occurs
+              -> (WebSocket -> CIO a)
+              -- ^ Computation using the WebSocket
+              -> CIO a
+withBinaryWebSocket url cb err f = do
+    result <- newEmptyMVar
+    let f' = WSComputation $ \ws -> concurrent $ f ws >>= putMVar result
+    liftIO $ newBin url cb' f' $ WSOnError $ concurrent $ err >>= putMVar result
+    takeMVar result
+  where
+    cb' = WSOnBinMsg $ \ws msg -> concurrent $ cb ws msg
+
 new :: URL
     -> WSOnMsg
     -> WSComputation
@@ -60,9 +84,30 @@ new = ffi "(function(url, cb, f, err) {\
              \return ws;\
            \})" 
 
+newBin :: URL
+    -> WSOnBinMsg
+    -> WSComputation
+    -> WSOnError
+    -> IO ()
+newBin = ffi "(function(url, cb, f, err) {\
+                \var ws = new WebSocket(url);\
+                \ws.binaryType = 'blob';\
+                \ws.onmessage = function(e) {A(cb,[ws,e.data,0]);};\
+                \ws.onopen = function(e) {A(f,[ws,0]);};\
+                \ws.onerror = function(e) {A(err,[0]);};\
+                \return ws;\
+              \})" 
+
 -- | Send a string over a WebSocket.
 wsSend :: WebSocket -> JSString -> CIO ()
-wsSend ws str = liftIO $ send ws str
+wsSend ws str = liftIO $ sendS ws str
 
-send :: WebSocket -> JSString -> IO ()
-send = ffi "(function(s, msg) {s.send(msg);})"
+-- | Send a Blob over a WebSocket.
+wsSendBlob :: WebSocket -> Blob -> CIO ()
+wsSendBlob ws b = liftIO $ sendB ws b
+
+sendS :: WebSocket -> JSString -> IO ()
+sendS = ffi "(function(s, msg) {s.send(msg);})"
+
+sendB :: WebSocket -> Blob -> IO ()
+sendB = ffi "(function(s, msg) {s.send(msg);})"
