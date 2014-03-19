2dadcb6701c56552735c18e98a1e8c9d7f6d3cde
Anton Ekblad <anton@ekblad.cc>
2014-03-19 19:51:12 +0100

    Turn version back to 0.2.99; some changes can't wait for 0.3-suitability.

--- src/Haste/Version.hs
+++ src/Haste/Version.hs
@@ -10,7 +10,7 @@ import Config (cProjectVersion)
 import Haste.Environment (hasteDir)
 
 hasteVersion :: Version
-hasteVersion = Version [0, 3] []
+hasteVersion = Version [0, 2, 99] []
 
 ghcVersion :: String
 ghcVersion = cProjectVersion
