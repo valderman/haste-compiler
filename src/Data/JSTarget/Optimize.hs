valderman/haste-compiler

commit 3f8e0a881bc6307dc8a3cd2a7d0e8cf1401ea5e1

Make Thunk and Eval explicit in IR.

src/Data/JSTarget/Optimize.hs
@@ -170,11 +170,9 @@ zapJSStringConversions ast =
            Call _ _ (Var (Foreign "unCStr")) [x]]) =
       return x
     opt (Call _ _ (Var (Foreign "toJSStr")) [
-           Call _ _ (Var (Foreign "E")) [
-             Call _ _ (Var (Foreign "unCStr")) [x]]]) =
+           Eval (Call _ _ (Var (Foreign "unCStr")) [x])]) =
       return x
-    opt (Call _ _ (Var (Foreign "new T"))
-         [Fun _ [] (Return x@(Call _ _ (Var (Foreign "unCStr")) [Lit _]))]) =
+    opt (Thunk (Return x@(Call _ _ (Var (Foreign "unCStr")) [Lit _]))) =
       return x
     opt x =
       return x
@@ -195,7 +193,7 @@ optimizeThunks :: JSTrav ast => ast -> TravM ast
 optimizeThunks ast =
     mapJS (const True) optEx return ast
   where
-    optEx (Call _ _ (Var (Foreign "E")) [x])
+    optEx (Eval x)
       | Just x' <- fromThunkEx x = return x'
       | Fun _ _ _ <- x           = return x
     optEx (Call arity calltype f args) | Just f' <- fromThunkEx f =
@@ -207,12 +205,8 @@ optimizeThunks ast =
 
 -- | Unpack the given expression if it's a thunk.
 fromThunk :: Exp -> Maybe Stm
-fromThunk (Call _ Fast (Var (Foreign "new T")) [body]) =
-  case body of
-    Fun Nothing [] (b) -> Just b
-    _                  -> Nothing
-fromThunk _ =
-  Nothing
+fromThunk (Thunk body) = Just body
+fromThunk _            = Nothing
 
 -- | Unpack the given expression if it's a thunk without internal bindings.
 fromThunkEx :: Exp -> Maybe Exp
@@ -235,12 +229,12 @@ computingThunk e =
 computingEx :: Exp -> Bool
 computingEx ex =
   case ex of
-    Var _                     -> False
-    Lit _                     -> False
-    Fun _ _ _                 -> False
-    Arr arr                   -> any computingEx arr
-    e | Just t <- fromThunk e -> False
-      | otherwise             -> True
+    Var _      -> False
+    Lit _      -> False
+    Fun _ _ _  -> False
+    Thunk _    -> False
+    Arr arr    -> any computingEx arr
+    _          -> True
 
 
 -- | Gather a map of all inlinable symbols; that is, the once that are used
@@ -409,5 +403,7 @@ tailLoopify f fun@(Fun mname args body) = do
     contains (Arr xs) var         = any (`contains` var) xs
     contains (AssignEx l r) var   = l `contains` var || r `contains` var
     contains (IfEx c t e) var     = any (`contains` var) [c,t,e]
+    contains (Eval x) var         = x `contains` var
+    contains (Thunk _) _          = False
 tailLoopify _ fun = do
   return fun

src/Data/JSTarget/AST.hs
@@ -85,6 +85,8 @@ data Exp where
   Arr       :: [Exp] -> Exp
   AssignEx  :: Exp -> Exp -> Exp
   IfEx      :: Exp -> Exp -> Exp -> Exp
+  Eval      :: Exp -> Exp
+  Thunk     :: Stm -> Exp
   deriving (Eq, Show)
 
 -- | Statements. The only mildly interesting thing here are the Case and Jump

src/Data/JSTarget/Binary.hs
@@ -60,21 +60,25 @@ instance Binary Exp where
   put (Arr exs)         = putWord8 7 >> put exs
   put (AssignEx l r)    = putWord8 8 >> put l >> put r
   put (IfEx c th el)    = putWord8 9 >> put c >> put th >> put el
+  put (Eval x)          = putWord8 10 >> put x
+  put (Thunk x)         = putWord8 11 >> put x
   
   get = do
     tag <- getWord8
     case tag of
-      0 -> Var <$> get
-      1 -> Lit <$> get
-      2 -> Not <$> get
-      3 -> BinOp <$> get <*> get <*> get
-      4 -> Fun <$> get <*> get <*> get
-      5 -> Call <$> get <*> get <*> get <*> get
-      6 -> Index <$> get <*> get
-      7 -> Arr <$> get
-      8 -> AssignEx <$> get <*> get
-      9 -> IfEx <$> get <*> get <*> get
-      n -> error $ "Bad tag in get :: Get Exp: " ++ show n
+      0  -> Var <$> get
+      1  -> Lit <$> get
+      2  -> Not <$> get
+      3  -> BinOp <$> get <*> get <*> get
+      4  -> Fun <$> get <*> get <*> get
+      5  -> Call <$> get <*> get <*> get <*> get
+      6  -> Index <$> get <*> get
+      7  -> Arr <$> get
+      8  -> AssignEx <$> get <*> get
+      9  -> IfEx <$> get <*> get <*> get
+      10 -> Eval <$> get
+      11 -> Thunk <$> get
+      n  -> error $ "Bad tag in get :: Get Exp: " ++ show n
 
 instance Binary Stm where
   put (Case e def alts next) =

src/Data/JSTarget/Constructors.hs
@@ -95,16 +95,12 @@ newVars prefix n =
 
 -- | Create a thunk.
 thunk :: AST Stm -> AST Exp
-thunk stm = callForeign "new T" [Fun Nothing [] <$> stm]
+thunk = fmap Thunk
 
 -- | Unpack the given expression if it's a thunk without internal bindings.
 fromThunk :: AST Exp -> Maybe (AST Exp)
-fromThunk (AST (Call 0 Fast (Var (Foreign "new T")) [body]) js) =
-  case body of
-    Fun Nothing [] (Return ex) -> Just (AST ex js)
-    _                          -> Nothing
-fromThunk _ =
-  Nothing
+fromThunk (AST (Thunk (Return ex)) js) = Just (AST ex js)
+fromThunk _                            = Nothing
 
 -- | Returns True if the given expression causes evaluation by appearing
 --   outside a closure, otherwise False.
@@ -119,10 +115,12 @@ evaluates (Index a b)    = evaluates a || evaluates b
 evaluates (Arr xs)       = any evaluates xs
 evaluates (AssignEx a b) = evaluates a || evaluates b
 evaluates (IfEx c a b)   = evaluates c || evaluates a || evaluates b
+evaluates (Thunk _)      = False
+evaluates (Eval x)       = True
 
 -- | Evaluate an expression that may or may not be a thunk.
 eval :: AST Exp -> AST Exp
-eval = callForeign "E" . (:[])
+eval = fmap Eval
 
 -- | A binary operator.
 binOp :: BinOp -> AST Exp -> AST Exp -> AST Exp

src/Data/JSTarget/Print.hs
@@ -102,6 +102,10 @@ instance Pretty Exp where
     pp l .+. sp .+. "=" .+. sp .+. pp r
   pp (IfEx c th el) = do
     pp c .+. sp .+. "?" .+. sp .+. pp th .+. sp .+. ":" .+. sp .+. pp el
+  pp (Eval x) = do
+    "E(" .+. pp x .+. ")"
+  pp (Thunk x) = do
+    "new T(function(){" .+. newl .+. indent (pp x) .+. "})"
 
 instance Pretty (Var, Exp) where
   pp (v, ex) = pp v .+. sp .+. "=" .+. sp .+. pp ex

src/Data/JSTarget/Traversal.hs
@@ -108,6 +108,10 @@ instance JSTrav Exp where
                            (acc'', th') <- mapEx acc' th
                            (acc''', el') <- mapEx acc'' el
                            return (acc''', IfEx c' th' el')
+                         Eval x         -> do
+                           fmap Eval <$> mapEx acc x
+                         Thunk x        -> do
+                           fmap Thunk <$> foldMapJS tr fe fs acc x
                      else do
                        return (acc, ast)
       fe acc' x
@@ -145,6 +149,10 @@ instance JSTrav Exp where
                     acc' <- foldJS tr f acc c
                     acc'' <- foldJS tr f acc' th
                     foldJS tr f acc'' el
+                  Eval ex       -> do
+                    foldJS tr f acc ex
+                  Thunk stm     -> do
+                    foldJS tr f acc stm
               else do
                 return acc
     f acc' expast
@@ -273,8 +281,10 @@ isShared :: ASTNode -> Bool
 isShared (Label _) = True
 isShared _         = False
 
+-- | Thunks and explicit lambdas count as lambda abstractions.
 isLambda :: ASTNode -> Bool
 isLambda (Exp (Fun _ _ _)) = True
+isLambda (Exp (Thunk _))   = True
 isLambda _                 = False
 
 isJump :: ASTNode -> Bool

src/Haste/PrimOps.hs
@@ -302,7 +302,7 @@ genOp cfg op xs =
     PopCnt16Op     -> Right $ callForeign "popCnt" [head xs]
     PopCnt32Op     -> Right $ callForeign "popCnt" [head xs]
     DelayOp        -> Right $ defState
-    SeqOp          -> Right $ callForeign "E" [head xs]
+    SeqOp          -> Right $ eval $ head xs
     AtomicallyOp   -> Right $ callSaturated (xs !! 0) []
     -- Get the data constructor tag from a value.
     DataToTagOp    -> callF "dataToTag"

commit f001169040af28a899303ab41b415bd501d21e22

Fix loops creating closures. It broke in 3f8e0a88.

src/Data/JSTarget/Optimize.hs
@@ -369,6 +369,7 @@ tailLoopify f fun@(Fun mname args body) = do
     -- Only traverse until we find a closure
     createsClosures = foldJS (\acc _ -> not acc) isClosure False
     isClosure _ (Exp (Fun _ _ _)) = pure True
+    isClosure _ (Exp (Thunk _))   = pure True
     isClosure acc _               = pure acc
 
     -- Assign any changed vars, then loop.