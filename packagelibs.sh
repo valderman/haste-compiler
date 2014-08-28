af0a01517afb0ea0b6248a5c34f79fa86f8b2721
Anton Ekblad <anton@ekblad.cc>
2014-08-28 08:49:15 +0100

    Update packagelibs.sh for 0.4.

--- packagelibs.sh
+++ packagelibs.sh
@@ -3,5 +3,5 @@
 if [[ $1 == "" ]] ; then
     echo "Usage: ./packagelibs.sh version"
 else
-    tar -cjf haste-libs-$1.tar.bz2 --exclude-backups '--exclude=*.hi' '--exclude=*.o' include libraries/{array/{array.cabal,Data,include,LICENSE,Setup.hs},base/{base.cabal,Control,Data,Debug,Foreign,Foreign.hs,GHC,Haste,include,LICENSE,Numeric.hs,Prelude.hs,Setup.hs,System,Text,Unsafe},ghc-prim/{ghc-prim.cabal,GHC,LICENSE,packageconfig},haste-lib/{haste-lib.cabal,src/{Haste.hs,Haste},LICENSE},integer-gmp/{integer-gmp.cabal,GHC,LICENSE,Setup.hs},rts.pkg}
+    tar -cjf haste-libs-$1.tar.bz2 --exclude-backups '--exclude=*.hi' '--exclude=*.o' include libraries/{array/{array.cabal,Data,include,LICENSE,Setup.hs},base-{ghc-7.6,ghc-7.8}/{base.cabal,Control,Data,Debug,Foreign,Foreign.hs,GHC,Haste,include,LICENSE,Numeric.hs,Prelude.hs,Setup.hs,System,Text,Unsafe},ghc-prim/{ghc-prim.cabal,GHC,LICENSE,packageconfig},haste-lib/{haste-lib.cabal,src/{Haste.hs,Haste},LICENSE},integer-gmp/{integer-gmp.cabal,GHC,LICENSE,Setup.hs},rts.pkg}
 fi
