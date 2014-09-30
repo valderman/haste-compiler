0d75b30bda82ef03c9a621df5642c983ed99c689
Anton Ekblad <anton@ekblad.cc>
2014-09-30 19:44:01 +0100

    Update list example for updated query selector API.

--- examples/list/list.hs
+++ examples/list/list.hs
@@ -3,15 +3,10 @@ import Haste
 
 main :: IO ()
 main = do
-  ul <- elemByQuerySelector document "ul#demo-list"
+  ul <- elemsByQS document "ul#demo-list"
   case ul of
-    Just el -> withQuerySelectorElems document "#demo-list li" (handleRemoves el)
-    _       -> error "Element 'ul#demo-list' not found"
-
-handleRemoves :: Elem -> [Elem] -> IO ()
-handleRemoves ul items = do
-  _ <- mapM (handleRemove ul) items
-  return ()
+    (el:_) -> mapQS_ document "#demo-list li" (handleRemove el)
+    _      -> error "Element 'ul#demo-list' not found"
 
 handleRemove :: Elem -> Elem -> IO Bool
 handleRemove ul li = do
