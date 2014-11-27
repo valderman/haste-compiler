d535fa7a9be5ed61441d11c92a2040da27001c5a
Anton Ekblad <anton@ekblad.cc>
2014-11-28 00:33:52 +0100

    Bump all versions to 0.5: the next release will have breaking changes.

--- src/Haste/Version.hs
+++ src/Haste/Version.hs
@@ -12,7 +12,7 @@ import Haste.Environment (hasteSysDir, ghcBinary)
 
 -- | Current Haste version.
 hasteVersion :: Version
-hasteVersion = Version [0, 4, 3] []
+hasteVersion = Version [0, 5, 0] []
 
 -- | Current Haste version as an Int. The format of this version number is
 --   MAJOR*10 000 + MINOR*100 + MICRO.
