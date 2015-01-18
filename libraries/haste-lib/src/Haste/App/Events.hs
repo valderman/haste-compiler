e58cb58cb90f4beb8f715451fe45417e02e93455
Anton Ekblad <anton@ekblad.cc>
2015-01-18 02:34:18 +0100

    Clean up Haste.App.Events.

--- libraries/haste-lib/src/Haste/App/Events.hs
+++ libraries/haste-lib/src/Haste/App/Events.hs
@@ -4,30 +4,16 @@
 --   the functions provided by this module rather than the ones from
 --   Haste.Events.
 module Haste.App.Events (
-    module Core,
-    ClientCallback, onEvent
+    module Events, onEvent
   ) where
 import Haste.App.Client
 import Haste.Concurrent
 import Haste.DOM.Core
-import qualified Haste.Events.Core as E
-import qualified Haste.Events.Core as Core hiding (onEvent)
-
--- | Bake a value of type a -> ... -> Client b into a -> ... -> IO b
-class ClientCallback a where
-  type T a
-  cbify :: ClientState -> a -> T a
-
-instance ClientCallback (Client ()) where
-  type T (Client ()) = IO ()
-  cbify cs = concurrent . runClientCIO cs
-
-instance ClientCallback b => ClientCallback (a -> b) where
-  type T (a -> b) = a -> T b
-  cbify cs f = \x -> cbify cs (f x)
+import qualified Haste.Events as E
+import qualified Haste.Events as Events hiding (onEvent)
 
 -- | Set a handler for a given event.
-onEvent :: (IsElem el, E.Event evt, ClientCallback a)
+onEvent :: (IsElem el, E.Event evt)
         => el
         -> evt
         -> (E.EventData evt -> Client ())
