9c676739addb4fade993e41fa58f33007791f0c3
Anton Ekblad <anton@ekblad.cc>
2015-02-25 00:47:45 +0100

    Fix strictness for Haste.Binary.Get.

--- libraries/haste-lib/src/Haste/Binary/Get.hs
+++ libraries/haste-lib/src/Haste/Binary/Get.hs
@@ -24,22 +24,26 @@ import qualified Control.Exception as Ex
 #endif
 
 #ifdef __HASTE__
-data Get a = Get {unG :: Unpacked -> Int -> Either String (Int, a)}
+data Result a = Ok !Int !a | Fail !String
+data Get a = Get {unG :: Unpacked -> Int -> Result a}
 
 instance Functor Get where
-  fmap f (Get m) = Get $ \buf next -> fmap (fmap f) (m buf next)
+  fmap f (Get m) = Get $ \buf next ->
+    case m buf next of
+      Ok next' x -> Ok next' (f x)
+      Fail s     -> Fail s
 
 instance Applicative Get where
   (<*>) = ap
   pure  = return
 
 instance Monad Get where
-  return x = Get $ \_ next -> Right (next, x)
+  return x = Get $ \_ next -> Ok next x
   (Get m) >>= f = Get $ \buf next ->
     case m buf next of
-      Right (next', x) -> unG (f x) buf next'
-      Left e           -> Left e
-  fail s = Get $ \_ _ -> Left s
+      Ok next' x -> unG (f x) buf next'
+      Fail e     -> Fail e
+  fail s = Get $ \_ _ -> Fail s
 
 {-# NOINLINE getW8 #-}
 getW8 :: Unpacked -> Int -> IO Word8
@@ -47,7 +51,7 @@ getW8 = ffi "(function(b,i){return b.getUint8(i);})"
 
 getWord8 :: Get Word8
 getWord8 =
-  Get $ \buf next -> Right (next+1, unsafePerformIO $ getW8 buf next)
+  Get $ \buf next -> Ok (next+1) (unsafePerformIO $ getW8 buf next)
 
 {-# NOINLINE getW16le #-}
 getW16le :: Unpacked -> Int -> IO Word16
@@ -55,7 +59,7 @@ getW16le = ffi "(function(b,i){return b.getUint16(i,true);})"
 
 getWord16le :: Get Word16
 getWord16le =
-  Get $ \buf next -> Right (next+2, unsafePerformIO $ getW16le buf next)
+  Get $ \buf next -> Ok (next+2) (unsafePerformIO $ getW16le buf next)
 
 {-# NOINLINE getW32le #-}
 getW32le :: Unpacked -> Int -> IO Word32
@@ -63,7 +67,7 @@ getW32le = ffi "(function(b,i){return b.getUint32(i,true);})"
 
 getWord32le :: Get Word32
 getWord32le =
-  Get $ \buf next -> Right (next+4, unsafePerformIO $ getW32le buf next)
+  Get $ \buf next -> Ok (next+4) (unsafePerformIO $ getW32le buf next)
 
 {-# NOINLINE getI8 #-}
 getI8 :: Unpacked -> Int -> IO Int8
@@ -71,7 +75,7 @@ getI8 = ffi "(function(b,i){return b.getInt8(i);})"
 
 getInt8 :: Get Int8
 getInt8 =
-  Get $ \buf next -> Right (next+1, unsafePerformIO $ getI8 buf next)
+  Get $ \buf next -> Ok (next+1) (unsafePerformIO $ getI8 buf next)
 
 {-# NOINLINE getI16le #-}
 getI16le :: Unpacked -> Int -> IO Int16
@@ -79,7 +83,7 @@ getI16le = ffi "(function(b,i){return b.getInt16(i,true);})"
 
 getInt16le :: Get Int16
 getInt16le =
-  Get $ \buf next -> Right (next+2, unsafePerformIO $ getI16le buf next)
+  Get $ \buf next -> Ok (next+2) (unsafePerformIO $ getI16le buf next)
 
 {-# NOINLINE getI32le #-}
 getI32le :: Unpacked -> Int -> IO Int32
@@ -87,7 +91,7 @@ getI32le = ffi "(function(b,i){return b.getInt32(i,true);})"
 
 getInt32le :: Get Int32
 getInt32le =
-  Get $ \buf next -> Right (next+4, unsafePerformIO $ getI32le buf next)
+  Get $ \buf next -> Ok (next+4) (unsafePerformIO $ getI32le buf next)
 
 {-# NOINLINE getF32le #-}
 getF32le :: Unpacked -> Int -> IO Float
@@ -95,7 +99,7 @@ getF32le = ffi "(function(b,i){return b.getFloat32(i,true);})"
 
 getFloat32le :: Get Float
 getFloat32le =
-  Get $ \buf next -> Right (next+4, unsafePerformIO $ getF32le buf next)
+  Get $ \buf next -> Ok (next+4) (unsafePerformIO $ getF32le buf next)
 
 {-# NOINLINE getF64le #-}
 getF64le :: Unpacked -> Int -> IO Double
@@ -103,15 +107,15 @@ getF64le = ffi "(function(b,i){return b.getFloat64(i,true);})"
 
 getFloat64le :: Get Double
 getFloat64le =
-  Get $ \buf next -> Right (next+8, unsafePerformIO $ getF64le buf next)
+  Get $ \buf next -> Ok (next+8) (unsafePerformIO $ getF64le buf next)
 
 getBytes :: Int -> Get BlobData
-getBytes len = Get $ \buf next -> Right (next+len, BlobData next len buf)
+getBytes len = Get $ \buf next -> Ok (next+len) (BlobData next len buf)
 
 -- | Read a 'JSString' of @n@ characters. Encoding is assumed to be UTF-16.
 getJSString :: Word32 -> Get JSString
 getJSString len = Get $ \buf next ->
-  Right (next+fromIntegral len, unsafePerformIO $ getJSS buf next len)
+  Ok (next+fromIntegral (len+len)) (unsafePerformIO $ getJSS buf next len)
 
 {-# NOINLINE getJSS #-}
 getJSS :: Unpacked -> Int -> Word32 -> IO JSString
@@ -119,15 +123,16 @@ getJSS = ffi "(function(b,off,len){return String.fromCharCode.apply(null,new Uin
 
 -- | Skip n bytes of input.
 skip :: Int -> Get ()
-skip len = Get $ \_buf next -> Right (next+len, ())
+skip len = Get $ \_buf next -> Ok (next+len) ()
 
 -- | Run a Get computation.
 runGet :: Get a -> BlobData -> Either String a
 runGet (Get p) (BlobData off len bd) = do
-    (consumed, x) <- p bd off
-    if consumed <= len
-      then Right x
-      else Left "Not enough data!"
+  case p bd off of
+    Ok consumed x
+      | consumed <= len -> Right x
+      | otherwise       -> Left "Not enough data!"
+    Fail s              -> Left s
 
 #else
 
