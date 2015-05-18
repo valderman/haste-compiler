3efd812668ac74c3060e9c164d4c5d30cc90baf4
Anton Ekblad <anton@ekblad.cc>
2015-05-18 13:21:39 +0200

    Add Haste.Performance interface to the Performance DOM interface.

---
+++ libraries/haste-lib/src/Haste/Performance.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- | (Very incomplete) Haste bindings to the @Performance@ DOM interface.
+module Haste.Performance (HRTimeStamp, now, navigationStart) where
+import Haste.Foreign
+
+type HRTimeStamp = Double
+
+-- | Returns the number of milliseconds since 'navigationStart', with
+--   (theoretically) microsecond precision.
+now :: IO HRTimeStamp
+now = ffi "(function(){return performance.now();})"
+
+-- | Returns the number of milliseconds elapsed since UNIX epoch at the moment
+--   when this document started loading.
+navigationStart :: IO Double
+navigationStart = ffi "(function(){return performance.timing.navigationStart;})"
