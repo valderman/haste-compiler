7b92aa8069fc1a5128fa530b8fd3a07b6107a32d
Anton Ekblad <anton@ekblad.cc>
2015-08-22 15:59:52 +0200

    Be explicit about re-exports in Haste.Foreign.

--- libraries/haste-lib/src/Haste/Foreign.hs
+++ libraries/haste-lib/src/Haste/Foreign.hs
@@ -1,3 +1,12 @@
 -- | High level JavaScript foreign interface.
-module Haste.Foreign (module Foreign) where
-import Haste.Prim.Foreign as Foreign
+module Haste.Foreign (
+    -- * Conversion to/from JSAny
+    ToAny (..), FromAny (..), JSAny,
+    Opaque, toOpaque, fromOpaque,
+    nullValue, toObject, has, get, index,
+
+    -- * Importing and exporting JavaScript functions
+    FFI, JSFunc,
+    ffi, constant, export
+  ) where
+import Haste.Prim.Foreign
