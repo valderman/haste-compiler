5fc84b810134f27948220f28f313b0b5ef7655b0
Anton Ekblad <anton@ekblad.cc>
2015-08-28 12:59:09 +0200

    Update packagelibs.sh.

--- packagelibs.sh
+++ packagelibs.sh
@@ -3,5 +3,6 @@
 if [[ $1 == "" ]] ; then
     echo "Usage: ./packagelibs.sh version"
 else
-    tar -cjf haste-libs-$1.tar.bz2 --exclude-backups '--exclude=*.hi' '--exclude=*.o' include utils/unlit/unlit.c libraries/rts.pkg libraries/haste-lib/{haste-lib.cabal,src/{Haste.hs,Haste},LICENSE} libraries/time libraries/ghc-7.8/{array/{array.cabal,Data,include,LICENSE,Setup.hs},base/{base.cabal,Control,Data,Debug,Foreign,Foreign.hs,GHC,Haste,include,LICENSE,Numeric.hs,Prelude.hs,Setup.hs,System,Text,Unsafe},ghc-prim/{ghc-prim.cabal,GHC,LICENSE,packageconfig},integer-gmp/{integer-gmp.cabal,GHC,LICENSE,Setup.hs}}
+    mkdir -p libraries/ghc-7.10/array/include
+    tar -cjf haste-libs-$1.tar.bz2 --exclude-backups '--exclude=*.hi' '--exclude=*.o' include utils/unlit/unlit.c libraries/rts.pkg libraries/haste-lib/{haste-lib.cabal,src/{Haste.hs,Haste},LICENSE} libraries/haste-prim/{src,LICENSE,Setup.hs,haste-prim.cabal} libraries/time/{lib,LICENSE,README,Setup.hs,time.cabal} libraries/ghc-{7.8,7.10}/{array/{array.cabal,Data,include,LICENSE,Setup.hs},base/{base.cabal,Control,Data,Debug,Foreign,Foreign.hs,GHC,Haste,include,LICENSE,Numeric*,Prelude.hs,Setup.hs,System,Text,Unsafe},ghc-prim/{ghc-prim.cabal,GHC,LICENSE,*.conf},integer-gmp/{integer-gmp.cabal,GHC,LICENSE,Setup.hs}}
 fi
