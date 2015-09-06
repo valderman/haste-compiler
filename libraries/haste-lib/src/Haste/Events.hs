03cc9482fcabf2053752f90d5af94f7ab0a70ede
Anton Ekblad <anton@ekblad.cc>
2015-09-06 11:30:03 +0200

    Explicitly export Haste.Events.*.* to make sure it gets into the docs.

--- libraries/haste-lib/src/Haste/Events.hs
+++ libraries/haste-lib/src/Haste/Events.hs
@@ -1,9 +1,18 @@
 -- | Event handling for Haste.
 module Haste.Events (
-    module Core,
-    module BasicEvents,
-    module KeyEvents,
-    module MouseEvents
+    -- * Core event functionality
+    Event (..), MonadEvent (..),
+    HandlerInfo,
+    unregisterHandler, onEvent, preventDefault,
+
+    -- * Basic events with no arguments
+    BasicEvent (..),
+
+    -- * Keyboard-related events
+    KeyEvent (..), KeyData (..), mkKeyData,
+
+    -- * Mouse-related events
+    MouseEvent (..), MouseData (..), MouseButton (..)
   ) where
 import Haste.Events.Core as Core
 import Haste.Events.BasicEvents as BasicEvents
