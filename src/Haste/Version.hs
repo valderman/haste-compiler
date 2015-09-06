7b663c9ce41b09cb8ce2f825e8a24b0df03cd8b3
Anton Ekblad <anton@ekblad.cc>
2015-09-06 02:18:27 +0200

    Bump all versions to 0.5.1.3.

--- src/Haste/Version.hs
+++ src/Haste/Version.hs
@@ -12,7 +12,7 @@ import Data.Maybe (listToMaybe)
 
 -- | Current Haste version.
 hasteVersion :: Version
-hasteVersion = Version [0,5,1,2] []
+hasteVersion = Version [0,5,1,3] []
 
 -- | Current Haste version as an Int. The format of this version number is
 --   MAJOR*10 000 + MINOR*100 + MICRO.
