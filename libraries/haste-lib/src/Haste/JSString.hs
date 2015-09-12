dd4c575a0939963962413cff410393cd5faf5d26
Anton Ekblad <anton@ekblad.cc>
2015-09-12 13:35:43 +0200

    Replace vanilla FFI with Haste.Foreign in Haste.JSString.

--- libraries/haste-lib/src/Haste/JSString.hs
+++ libraries/haste-lib/src/Haste/JSString.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -fno-warn-unused-binds #-}
-{-# LANGUAGE OverloadedStrings, ForeignFunctionInterface, CPP, MagicHash #-}
+{-# LANGUAGE OverloadedStrings, ForeignFunctionInterface, CPP, MagicHash,
+             GeneralizedNewtypeDeriving #-}
 -- | JSString standard functions, to make them a more viable alternative to
 --   the horribly inefficient standard Strings.
 --
@@ -38,27 +39,54 @@ import System.IO.Unsafe
 d2c :: Double -> Char
 d2c d = unsafeCoerce# d
 
-foreign import ccall _jss_singleton :: Char -> JSString
-foreign import ccall _jss_cons :: Char -> JSString -> JSString
-foreign import ccall _jss_snoc :: JSString -> Char -> JSString
-foreign import ccall _jss_append :: JSString -> JSString -> JSString
-foreign import ccall _jss_len :: JSString -> Int
-foreign import ccall _jss_index :: JSString -> Int -> Double
-foreign import ccall _jss_substr :: JSString -> Int -> JSString
-foreign import ccall _jss_take :: Int -> JSString -> JSString
-foreign import ccall _jss_rev :: JSString -> JSString
-foreign import ccall _jss_re_match :: JSString -> RegEx -> Bool
-foreign import ccall _jss_re_compile :: JSString -> JSString -> RegEx
-foreign import ccall _jss_re_replace :: JSString -> RegEx -> JSString -> JSString
-foreign import ccall _jss_re_find :: RegEx -> JSString -> Ptr [JSString]
+_jss_singleton :: Char -> IO JSString
+_jss_singleton = ffi "String.fromCharCode"
+
+_jss_cons :: Char -> JSString -> IO JSString
+_jss_cons = ffi "(function(c,s){return String.fromCharCode(c)+s;})"
+
+_jss_snoc :: JSString -> Char -> IO JSString
+_jss_snoc = ffi "(function(s,c){return s+String.fromCharCode(c);})"
+
+_jss_append :: JSString -> JSString -> IO JSString
+_jss_append = ffi "(function(a,b){return a+b;})"
+
+_jss_len :: JSString -> IO Int
+_jss_len = ffi "(function(s){return s.length;})"
+
+_jss_index :: JSString -> Int -> IO Double
+_jss_index = ffi "(function(s,i){return s.charCodeAt(i);})"
+
+_jss_substr :: JSString -> Int -> IO JSString
+_jss_substr = ffi "(function(s,x){return s.substr(x);})"
+
+_jss_take :: Int -> JSString -> IO JSString
+_jss_take = ffi "(function(n,s){return s.substr(0,n);})"
+
+_jss_rev :: JSString -> IO JSString
+_jss_rev = ffi "(function(s){return s.split('').reverse().join('');})"
+
+_jss_re_match :: JSString -> RegEx -> IO Bool
+_jss_re_match = ffi "(function(s,re){return s.search(re)>=0;})"
+
+_jss_re_compile :: JSString -> JSString -> IO RegEx
+_jss_re_compile = ffi "(function(re,fs){return new RegExp(re,fs);})"
+
+_jss_re_replace :: JSString -> RegEx -> JSString -> IO JSString
+_jss_re_replace = ffi "(function(s,re,rep){return s.replace(re,rep);})"
+
+_jss_re_find :: RegEx -> JSString -> IO [JSString]
+_jss_re_find = ffi "(function(re,s) {\
+var a = s.match(re);\
+return a ? a : [];})"
 
 {-# INLINE _jss_map #-}
 _jss_map :: (Char -> Char) -> JSString -> JSString
-_jss_map f = _jss_cmap (_jss_singleton . f)
+_jss_map f s = veryUnsafePerformIO $ cmap_js (_jss_singleton . f) s
 
 {-# INLINE _jss_cmap #-}
 _jss_cmap :: (Char -> JSString) -> JSString -> JSString
-_jss_cmap f s = unsafePerformIO $ cmap_js (return . f) s
+_jss_cmap f s = veryUnsafePerformIO $ cmap_js (return . f) s
 
 cmap_js :: (Char -> IO JSString) -> JSString -> IO JSString
 cmap_js = ffi "(function(f,s){\
@@ -104,29 +132,29 @@ return x;})"
 d2c :: Char -> Char
 d2c = id
 
-_jss_singleton :: Char -> JSString
-_jss_singleton c = toJSStr [c]
+_jss_singleton :: Char -> IO JSString
+_jss_singleton c = return $ toJSStr [c]
 
-_jss_cons :: Char -> JSString -> JSString
-_jss_cons c s = toJSStr (c : fromJSStr s)
+_jss_cons :: Char -> JSString -> IO JSString
+_jss_cons c s = return $ toJSStr (c : fromJSStr s)
 
-_jss_snoc :: JSString -> Char -> JSString
-_jss_snoc s c = toJSStr (fromJSStr s ++ [c])
+_jss_snoc :: JSString -> Char -> IO JSString
+_jss_snoc s c = return $ toJSStr (fromJSStr s ++ [c])
 
-_jss_append :: JSString -> JSString -> JSString
-_jss_append a b = catJSStr "" [a, b]
+_jss_append :: JSString -> JSString -> IO JSString
+_jss_append a b = return $ catJSStr "" [a, b]
 
-_jss_len :: JSString -> Int
-_jss_len s = Data.List.length $ fromJSStr s
+_jss_len :: JSString -> IO Int
+_jss_len s = return $ Data.List.length $ fromJSStr s
 
-_jss_index :: JSString -> Int -> Char
-_jss_index s n = fromJSStr s !! n
+_jss_index :: JSString -> Int -> IO Char
+_jss_index s n = return $ fromJSStr s !! n
 
-_jss_substr :: JSString -> Int -> JSString
-_jss_substr s n = toJSStr $ Data.List.drop n $ fromJSStr s
+_jss_substr :: JSString -> Int -> IO JSString
+_jss_substr s n = return $ toJSStr $ Data.List.drop n $ fromJSStr s
 
-_jss_take :: Int -> JSString -> JSString
-_jss_take n = toJSStr . Data.List.take n . fromJSStr
+_jss_take :: Int -> JSString -> IO JSString
+_jss_take n = return . toJSStr . Data.List.take n . fromJSStr
 
 _jss_map :: (Char -> Char) -> JSString -> JSString
 _jss_map f = toJSStr . Data.List.map f . fromJSStr
@@ -135,8 +163,8 @@ _jss_cmap :: (Char -> JSString) -> JSString -> JSString
 _jss_cmap f =
   toJSStr . Data.List.concat . Data.List.map (fromJSStr . f) . fromJSStr
 
-_jss_rev :: JSString -> JSString
-_jss_rev = toJSStr . Data.List.reverse . fromJSStr
+_jss_rev :: JSString -> IO JSString
+_jss_rev = return . toJSStr . Data.List.reverse . fromJSStr
 
 _jss_foldl :: (a -> Char -> a) -> a -> JSString -> a
 _jss_foldl f x = Data.List.foldl' f x . fromJSStr
@@ -144,19 +172,19 @@ _jss_foldl f x = Data.List.foldl' f x . fromJSStr
 _jss_foldr :: (Char -> a -> a) -> a -> JSString -> a
 _jss_foldr f x = Data.List.foldr f x . fromJSStr
 
-_jss_re_compile :: JSString -> JSString -> RegEx
+_jss_re_compile :: JSString -> JSString -> IO RegEx
 _jss_re_compile _ _ =
   error "Regular expressions are only supported client-side!"
 
-_jss_re_match :: JSString -> RegEx -> Bool
+_jss_re_match :: JSString -> RegEx -> IO Bool
 _jss_re_match _ _ =
   error "Regular expressions are only supported client-side!"
 
-_jss_re_replace :: JSString -> RegEx -> JSString -> JSString
+_jss_re_replace :: JSString -> RegEx -> JSString -> IO JSString
 _jss_re_replace _ _ _ =
   error "Regular expressions are only supported client-side!"
 
-_jss_re_find :: RegEx -> JSString -> Ptr [JSString]
+_jss_re_find :: RegEx -> JSString -> IO [JSString]
 _jss_re_find _ _ =
   error "Regular expressions are only supported client-side!"
 
@@ -164,9 +192,10 @@ _jss_re_find _ _ =
 
 -- | A regular expression. May be used to match and replace JSStrings.
 newtype RegEx = RegEx JSAny
+  deriving (ToAny, FromAny)
 
 instance IsString RegEx where
-  fromString s = _jss_re_compile (fromString s) ""
+  fromString s = veryUnsafePerformIO $ _jss_re_compile (fromString s) ""
 
 -- | O(1) The empty JSString.
 empty :: JSString
@@ -174,7 +203,7 @@ empty = ""
 
 -- | O(1) JSString consisting of a single character.
 singleton :: Char -> JSString
-singleton = _jss_singleton
+singleton = veryUnsafePerformIO . _jss_singleton
 
 -- | O(n) Convert a list of Char into a JSString.
 pack :: [Char] -> JSString
@@ -187,22 +216,22 @@ unpack = fromJSStr
 infixr 5 `cons`
 -- | O(n) Prepend a character to a JSString.
 cons :: Char -> JSString -> JSString
-cons = _jss_cons
+cons c s = veryUnsafePerformIO $ _jss_cons c s
 
 infixl 5 `snoc`
 -- | O(n) Append a character to a JSString.
 snoc :: JSString -> Char -> JSString
-snoc = _jss_snoc
+snoc s c = veryUnsafePerformIO $ _jss_snoc s c
 
 -- | O(n) Append two JSStrings.
 append :: JSString -> JSString -> JSString
-append = _jss_append
+append a b = veryUnsafePerformIO $ _jss_append a b
 
 -- | O(1) Extract the first element of a non-empty JSString.
 head :: JSString -> Char
 head s =
 #ifdef __HASTE__
-  case _jss_index s 0 of
+  case veryUnsafePerformIO $ _jss_index s 0 of
     c | isNaN c   -> error "Haste.JSString.head: empty JSString"
       | otherwise -> d2c c -- Double/Int/Char share representation.
 #else
@@ -212,43 +241,43 @@ head s =
 -- | O(1) Extract the last element of a non-empty JSString.
 last :: JSString -> Char
 last s =
-  case _jss_len s of
+  case veryUnsafePerformIO $ _jss_len s of
     0 -> error "Haste.JSString.head: empty JSString"
-    n -> d2c (_jss_index s (n-1))
+    n -> d2c (veryUnsafePerformIO $ _jss_index s (n-1))
 
 -- | O(n) All elements but the first of a JSString. Returns an empty JSString
 --   if the given JSString is empty.
 tail :: JSString -> JSString
-tail s = _jss_substr s 1
+tail s = veryUnsafePerformIO $ _jss_substr s 1
 
 -- | O(n) Drop 'n' elements from the given JSString.
 drop :: Int -> JSString -> JSString
-drop n s = _jss_substr s (max 0 n)
+drop n s = veryUnsafePerformIO $ _jss_substr s (max 0 n)
 
 -- | O(n) Take 'n' elements from the given JSString.
 take :: Int -> JSString -> JSString
-take n s = _jss_take n s
+take n s = veryUnsafePerformIO $ _jss_take n s
 
 -- | O(n) All elements but the last of a JSString. Returns an empty JSString
 --   if the given JSString is empty.
 init :: JSString -> JSString
-init s = _jss_take (_jss_len s-1) s
+init s = veryUnsafePerformIO $ _jss_take (veryUnsafePerformIO (_jss_len s)-1) s
 
 -- | O(1) Test whether a JSString is empty.
 null :: JSString -> Bool
-null s = _jss_len s == 0
+null s = veryUnsafePerformIO (_jss_len s) == 0
 
 -- | O(1) Get the length of a JSString as an Int.
 length :: JSString -> Int
-length s = _jss_len s
+length = veryUnsafePerformIO . _jss_len
 
 -- | O(n) Map a function over the given JSString.
 map :: (Char -> Char) -> JSString -> JSString
-map = _jss_map
+map f s = _jss_map f s
 
 -- | O(n) reverse a JSString.
 reverse :: JSString -> JSString
-reverse = _jss_rev
+reverse = veryUnsafePerformIO . _jss_rev
 
 -- | O(n) Join a list of JSStrings, with a specified separator. Equivalent to
 --   'String.join'.
@@ -293,11 +322,11 @@ splitAt n s = (Haste.JSString.take n s, Haste.JSString.drop n s)
 -- | O(n) Determines whether the given JSString matches the given regular
 --   expression or not.
 matches :: JSString -> RegEx -> Bool
-matches = _jss_re_match
+matches s re = veryUnsafePerformIO $ _jss_re_match s re
 
 -- | O(n) Find all strings corresponding to the given regular expression.
 match :: RegEx -> JSString -> [JSString]
-match re s = fromPtr $ _jss_re_find re s
+match re s = veryUnsafePerformIO $ _jss_re_find re s
 
 -- | O(n) Compile a regular expression and an (optionally empty) list of flags
 --   into a 'RegEx' which can be used to match, replace, etc. on JSStrings.
@@ -308,11 +337,11 @@ match re s = fromPtr $ _jss_re_find re s
 regex :: JSString -- ^ Regular expression.
       -> JSString -- ^ Potential flags.
       -> RegEx
-regex re flags = _jss_re_compile re flags
+regex re flags = veryUnsafePerformIO $ _jss_re_compile re flags
 
 -- | O(n) String substitution using regular expressions.
 replace :: JSString -- ^ String perform substitution on.
         -> RegEx    -- ^ Regular expression to match.
         -> JSString -- ^ Replacement string.
         -> JSString
-replace = _jss_re_replace
+replace s re rep = veryUnsafePerformIO $ _jss_re_replace s re rep
