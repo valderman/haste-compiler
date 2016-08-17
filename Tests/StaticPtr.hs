1a26a74057ba35d013d502a4c38ee4f7708d91ad
Anton Ekblad <anton@ekblad.cc>
2016-08-17 16:22:35 +0200

    tests: update StaticPointers test with dereferencing.

--- Tests/StaticPtr.hs
+++ Tests/StaticPtr.hs
@@ -2,7 +2,24 @@
 module Tests.StaticPtr where
 import GHC.StaticPtr
 
+{-# NOINLINE ptr #-}
+ptr :: StaticPtr String
+ptr = static "This is a "
+
+{-# NOINLINE ptr2 #-}
+ptr2 :: StaticPtr String
+ptr2 = static "string which is"
+
+{-# NOINLINE ptr3 #-}
+ptr3 :: StaticPtr String
+ptr3 = static " statically pointed to."
+
+getptr :: StaticPtr a -> IO (Maybe a)
+getptr = fmap (fmap deRefStaticPtr) . unsafeLookupStaticPtr . staticKey
+
 runTest :: IO ()
 runTest = do
   print (staticKey (static staticPtrKeys))
   print (staticKey (static runTest))
+  [Just x, Just z, Just y] <- mapM getptr [ptr, ptr3, ptr2]
+  putStrLn $ concat [x, y, z]
