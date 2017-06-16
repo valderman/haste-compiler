eb37c6f1ba76fc35fce67dae82ca100445461423
Anton Ekblad <anton@ekblad.cc>
2017-06-16 17:20:25 +0200

    Use proper concurrency on the server for Haste.Concurrent.

--- libraries/haste-lib/src/Haste/Concurrent/Monad.hs
+++ libraries/haste-lib/src/Haste/Concurrent/Monad.hs
@@ -1,50 +1,74 @@
 {-# LANGUAGE GADTs, TypeFamilies, FlexibleInstances, FlexibleContexts, CPP #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Implements concurrency for Haste based on "A Poor Man's Concurrency Monad".
 module Haste.Concurrent.Monad (
-    MVar, CIO, ToConcurrent (..), MonadConc (..),
+    MVar, CIO, MonadConc (..),
     forkIO, forkMany, newMVar, newEmptyMVar, takeMVar, putMVar, withMVarIO,
-    peekMVar, modifyMVarIO, readMVar, concurrent, liftIO,
+    modifyMVarIO, readMVar, concurrent, liftIO,
     tryTakeMVar, tryPutMVar
   ) where
 import Control.Monad.IO.Class
-import Control.Monad.Cont.Class
 import Control.Monad
 import Data.IORef
 import Haste.Events.Core (MonadEvent (..))
 
--- | Any monad which supports concurrency.
-class MonadIO m => MonadConc m where
-  liftCIO :: CIO a -> m a
-  fork    :: m () -> m ()
+#ifndef __HASTE__
 
--- | Old name for 'liftCIO'.
-{-# DEPRECATED liftConc "liftConc is deprecated; use liftCIO instead" #-}
-liftConc :: MonadConc m => CIO a -> m a
-liftConc = liftCIO
+-- Running native: proper concurrency
 
-instance MonadConc CIO where
-  liftCIO = id
-  fork = forkIO
+import qualified Control.Concurrent as CC
 
-instance MonadEvent CIO where
-  mkHandler = return . fmap concurrent
+-- | Concurrent IO monad. The normal IO monad does not have concurrency
+--   capabilities with Haste. This monad is basically IO plus concurrency.
+newtype CIO a = CIO (IO a)
+  deriving (Functor, Applicative, Monad, MonadIO)
 
--- | Embed concurrent computations into non-concurrent ones.
-class ToConcurrent a where
-  type Async a
-  async :: Async a -> a
+newtype MVar a = MVar {unV :: CC.MVar a}
 
-instance ToConcurrent (IO ()) where
-  type Async (IO ()) = CIO ()
-  async = concurrent
+-- | Run a concurrent computation. Two different concurrent computations may
+--   share MVars; if this is the case, then a call to `concurrent` may return
+--   before all the threads it spawned finish executing.
+concurrent :: CIO () -> IO ()
+concurrent (CIO m) = m
 
-instance ToConcurrent (CIO ()) where
-  type Async (CIO ()) = CIO ()
-  async = id
+-- | Spawn a new thread.
+forkIO :: CIO () -> CIO ()
+forkIO (CIO m) = CIO $ void $ CC.forkIO m
 
-instance ToConcurrent b => ToConcurrent (a -> b) where
-  type Async (a -> b) = a -> Async b
-  async f = \x -> async (f x)
+-- | Spawn several threads at once.
+forkMany :: [CIO ()] -> CIO ()
+forkMany = mapM_ forkIO
+
+-- | Create a new MVar with an initial value.
+newMVar :: MonadIO m => a -> m (MVar a)
+newMVar = fmap MVar . liftIO . CC.newMVar
+
+-- | Create a new empty MVar.
+newEmptyMVar :: MonadIO m => m (MVar a)
+newEmptyMVar = MVar <$> liftIO CC.newEmptyMVar
+
+-- | Read an MVar. Blocks if the MVar is empty.
+--   Only the first writer in the write queue, if any, is woken.
+takeMVar :: MonadConc m => MVar a -> m a
+takeMVar = liftIO . CC.takeMVar . unV
+
+-- | Try to take a value from an MVar, but return @Nothing@ if it is empty.
+tryTakeMVar :: MonadConc m => MVar a -> m (Maybe a)
+tryTakeMVar = liftIO . CC.tryTakeMVar . unV
+
+-- | Write an MVar. Blocks if the MVar is already full.
+--   Only the first reader in the read queue, if any, is woken.
+putMVar :: MonadConc m => MVar a -> a -> m ()
+putMVar (MVar v) x = liftIO $ CC.putMVar v x
+
+-- | Try to put a value into an MVar, returning @False@ if the MVar is already
+--   full.
+tryPutMVar :: MonadConc m => MVar a -> a -> m Bool
+tryPutMVar (MVar v) x = liftIO $ CC.tryPutMVar v x
+#else
+
+-- Running in Haste: poor man's concurrency
+-- See native version for docs.
 
 data MV a
   = Full a [(a, CIO ())] -- A full MVar: a queue of writers
@@ -56,8 +80,6 @@ data Action where
   Fork :: [Action] -> Action
   Stop :: Action
 
--- | Concurrent IO monad. The normal IO monad does not have concurrency
---   capabilities with Haste. This monad is basically IO plus concurrency.
 newtype CIO a = C {unC :: (a -> Action) -> Action}
 
 instance Monad CIO where
@@ -76,27 +98,20 @@ instance Applicative CIO where
 instance MonadIO CIO where
   liftIO m = C $ \next -> Atom (fmap next m)
 
-instance MonadCont CIO where
-  callCC f = C $ \next -> unC (f (\a -> C $ \_ -> next a)) next
+callCC f = C $ \next -> unC (f (\a -> C $ \_ -> next a)) next
 
--- | Spawn a new thread.
 forkIO :: CIO () -> CIO ()
 forkIO (C m) = C $ \next -> Fork [next (), m (const Stop)]
 
--- | Spawn several threads at once.
 forkMany :: [CIO ()] -> CIO ()
 forkMany ms = C $ \next -> Fork (next () : [act (const Stop) | C act <- ms])
 
--- | Create a new MVar with an initial value.
 newMVar :: MonadIO m => a -> m (MVar a)
 newMVar a = liftIO $ MVar `fmap` newIORef (Full a [])
 
--- | Create a new empty MVar.
 newEmptyMVar :: MonadIO m => m (MVar a)
 newEmptyMVar = liftIO $ MVar `fmap` newIORef (Empty [])
 
--- | Read an MVar. Blocks if the MVar is empty.
---   Only the first writer in the write queue, if any, is woken.
 takeMVar :: MonadConc m => MVar a -> m a
 takeMVar (MVar ref) = liftCIO $ do
   callCC $ \next -> join $ liftIO $ do
@@ -112,7 +127,6 @@ takeMVar (MVar ref) = liftCIO $ do
         writeIORef ref (Empty (rs ++ [next]))
         return $ C (const Stop)
 
--- | Try to take a value from an MVar, but return @Nothing@ if it is empty.
 tryTakeMVar :: MonadConc m => MVar a -> m (Maybe a)
 tryTakeMVar (MVar ref) = liftCIO $ do
   join $ liftIO $ do
@@ -127,25 +141,6 @@ tryTakeMVar (MVar ref) = liftCIO $ do
       Empty rs -> do
         return $ return Nothing
 
--- | Peek at the value inside a given MVar, if any, without removing it.
-peekMVar :: MonadIO m => MVar a -> m (Maybe a)
-peekMVar (MVar ref) = liftIO $ do
-  v <- readIORef ref
-  case v of
-    Full x _ -> return (Just x)
-    _        -> return Nothing
-
--- | Read an MVar then put it back. As Javascript is single threaded, this
---   function is atomic. If this ever changes, this function will only be
---   atomic as long as no other thread attempts to write to the MVar.
-readMVar :: MonadConc m => MVar a -> m a
-readMVar m = do
-  x <- takeMVar m
-  putMVar m x
-  return x
-
--- | Write an MVar. Blocks if the MVar is already full.
---   Only the first reader in the read queue, if any, is woken.
 putMVar :: MonadConc m => MVar a -> a -> m ()
 putMVar (MVar ref) x = liftCIO $ do
   callCC $ \next -> join $ liftIO $ do
@@ -161,8 +156,6 @@ putMVar (MVar ref) x = liftCIO $ do
         writeIORef ref (Full x [])
         return $ return ()
 
--- | Try to put a value into an MVar, returning @False@ if the MVar is already
---   full.
 tryPutMVar :: MonadConc m => MVar a -> a -> m Bool
 tryPutMVar (MVar ref) x = liftCIO $ do
   join $ liftIO $ do
@@ -177,20 +170,6 @@ tryPutMVar (MVar ref) x = liftCIO $ do
         writeIORef ref (Full x [])
         return $ return True
 
--- | Perform an IO action over an MVar.
-withMVarIO :: MonadConc m => MVar a -> (a -> IO b) -> m b
-withMVarIO v m = takeMVar v >>= liftIO . m
-
--- | Perform an IO action over an MVar, then write the MVar back.
-modifyMVarIO :: MonadConc m => MVar a -> (a -> IO (a, b)) -> m b
-modifyMVarIO v m = do
-  (x, res) <- withMVarIO v m
-  putMVar v x
-  return res
-
--- | Run a concurrent computation. Two different concurrent computations may
---   share MVars; if this is the case, then a call to `concurrent` may return
---   before all the threads it spawned finish executing.
 concurrent :: CIO () -> IO ()
 concurrent (C m) = scheduler [m (const Stop)]
   where
@@ -205,3 +184,36 @@ concurrent (C m) = scheduler [m (const Stop)]
           scheduler ps
     scheduler _ =
       return ()
+#endif
+
+-- | Any monad which supports concurrency.
+class MonadIO m => MonadConc m where
+  liftCIO :: CIO a -> m a
+  fork    :: m () -> m ()
+
+instance MonadConc CIO where
+  liftCIO = id
+  fork = forkIO
+
+instance MonadEvent CIO where
+  mkHandler = return . fmap concurrent
+
+-- | Read an MVar then put it back. As Javascript is single threaded, this
+--   function is atomic. If this ever changes, this function will only be
+--   atomic as long as no other thread attempts to write to the MVar.
+readMVar :: MonadConc m => MVar a -> m a
+readMVar m = do
+  x <- takeMVar m
+  putMVar m x
+  return x
+
+-- | Perform an IO action over an MVar.
+withMVarIO :: MonadConc m => MVar a -> (a -> IO b) -> m b
+withMVarIO v m = takeMVar v >>= liftIO . m
+
+-- | Perform an IO action over an MVar, then write the MVar back.
+modifyMVarIO :: MonadConc m => MVar a -> (a -> IO (a, b)) -> m b
+modifyMVarIO v m = do
+  (x, res) <- withMVarIO v m
+  putMVar v x
+  return res
